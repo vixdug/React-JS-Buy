eb664d542691a39140604634dff97abe2fde6906
Victoria <victoria.duggan@shopify.com>
2019-04-13 11:48:43 -0700

    set up Redux and state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ButtonExampleLabeledBasic from './components/ButtonExampleLabeledBasic'
+import Cart from './components/shopify/Cart';
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.updateQuantityInCart = this.updateQuantityInCart.bind(this);
+    this.removeLineItemInCart = this.removeLineItemInCart.bind(this);
+    this.handleCartClose = this.handleCartClose.bind(this);
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
+        <Cart
+    checkout={this.state.checkout}
+    isCartOpen={this.state.isCartOpen}
+    handleCartClose={this.handleCartClose}
+    updateQuantityInCart={this.updateQuantityInCart}
+    removeLineItemInCart={this.removeLineItemInCart}
+ />
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -5,14 +5,21 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import Client from 'shopify-buy';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const client = Client.buildClient({
     storefrontAccessToken: 'cebcc0633d451fc301777269dd25cd83',
     domain: 'plant-dyed-goods.myshopify.com'
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,  
+    document.getElementById('root')
+  );
+  
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
